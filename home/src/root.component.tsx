854c26c0a440a570f63ec392fe42875ed6dfe46f
mikand13 <mikkelsen.anders@gmail.com>
2020-10-13 11:28:48 +0200

    use react router

--- home/src/root.component.tsx
+++ home/src/root.component.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 import { links } from "./root.helper";
 
 export default function Root(props) {
   return (
     <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
       <div className="flex items-center justify-between">
-        {links.map((link) => {
-          return (
-            <div key={link.href}>
-              <a href={link.href} className="p-6">
-                {link.name}
-              </a>
-            </div>
-          );
-        })}
+        <BrowserRouter basename="/">
+          {links.map((link) => {
+            return (
+              <div key={link.href}>
+                <Link to={link.href} className="p-6">
+                  {link.name}
+                </Link>
+              </div>
+            );
+          })}
+        </BrowserRouter>
       </div>
     </div>
   );
